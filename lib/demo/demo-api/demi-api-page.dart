b9805a0317473c33faead5a4cabcfbdaf39b25a9
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-05-05 12:20:33 +0200

    Demo API

--- lib/demo/demo-api/demi-api-page.dart
+++ lib/demo/demo-api/demi-api-page.dart
@@ -1,7 +1,30 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:eni_demo/demo/demo-api/article.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:sn_progress_dialog/sn_progress_dialog.dart';
 
+// Ma ligne widget d'article
+class ArticleItemView extends StatelessWidget {
+  // Un article dans la ligne
+  Article article;
+
+  ArticleItemView(this.article, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFFDCE5E3),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text('Id : ${article.id} - ${article.title}'),
+      ),
+    );
+  }
+}
+
+// Statefull widget
 class DemoApiPage extends StatefulWidget {
   @override
   State<DemoApiPage> createState() {
@@ -10,7 +33,16 @@ class DemoApiPage extends StatefulWidget {
 }
 
 class DemiApiPageState extends State<DemoApiPage> {
+  List<Article> articles = [];
+
   void callApi() async {
+    // Affiche une dialog box
+    ProgressDialog pd = ProgressDialog(context: context);
+    pd.show(msg: 'Articles en cours de chargement...');
+
+    // Simule du temps de chargement
+    await Future<void>.delayed(const Duration(seconds: 1));
+
     // Récupère la réponse de la requête HTTP
     var response = await http.get(Uri.parse(
         "https://raw.githubusercontent.com/Chocolaterie/EniFlutterHCDA091/master/api/get-articles.json"));
@@ -24,7 +56,22 @@ class DemiApiPageState extends State<DemoApiPage> {
       Map<String, dynamic> json = decoder.convert(response.body);
 
       // Transformer ce JSON en article
-      print("Fini");
+
+      var articlesJson = (json['articles'] as List);
+
+      // Pour chaque iteration dans le json -> ajouter dans la liste un article
+      List<Article> articlesToMap = [];
+      for (var articleJson in articlesJson) {
+        articlesToMap.add(Article.fromJson(articleJson));
+      }
+
+      // Mettre à jour l'ecran avec les articles récupérés
+      setState(() {
+        articles = articlesToMap;
+
+        // Fermer la dialog box
+        pd.close();
+      });
     }
   }
 
@@ -36,14 +83,27 @@ class DemiApiPageState extends State<DemoApiPage> {
       body: Flex(
         direction: Axis.vertical,
         children: [
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () {
-                  callApi();
-                },
-                child: const Text("Appeler l'API")),
-          )
+          // Mon bouton call API
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    callApi();
+                  },
+                  child: const Text("Appeler l'API")),
+            ),
+          ),
+
+          // Ma ListView d'article
+          Expanded(
+            child: ListView.builder(
+                itemCount: articles.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ArticleItemView(articles[index]);
+                }),
+          ),
         ],
       ),
     );
